32755c6a422417f6ac0ba71cf9defbe87054085e
kaavyashankar <60006441+kaavyashankar@users.noreply.github.com>
2020-12-23 19:34:01 +0530

    First commit

---
+++ projfrontend/src/Routes.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
+
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Cart from "./core/Cart";
+
+
+import UserDashboard from "./user/UserDashboard";
+
+
+const Routes =  () => {
+    return(
+        <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/signin" exact component={Signin} />
+            <Route path="/cart" exact component={Cart} />
+
+            <PrivateRoutes path ="/user/dashboard" exact component={UserDashboard} />
+           
+            
+        </Switch>
+        </BrowserRouter>
+    )
+};
+
+export default Routes;
---
+++ projfrontend/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Routes from "./Routes";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
+
+
+
+
+
+serviceWorkerRegistration.register();
